869b2e2da90058df0a58550ef328b070c4bffc4b
Fuzail Ahemd <fuzailcode@gmail.com>
2025-11-28 22:17:07 +0500

    my assistant

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import React, { useContext, useState } from 'react'
+import assistantImg from './assets/ai.png'
+import { CiMicrophoneOn } from 'react-icons/ci'
+import { AssistantContext } from './context/AssistantContext'
+import listeningSvg from "./assets/speak.gif"
+import aiVoice from "./assets/aiVoice.gif"
+
+
+const App = () => {
+
+
+  const { recognition, speaking } = useContext(AssistantContext)
+  const [listening, setListening] = useState(false);
+
+  const handleClick = async () => {
+    // speak("hello fuzail how are you");
+    recognition.start()
+    // askQuestion()
+  }
+
+  recognition.onstart = () => {
+    setListening(true);   // start → TRUE
+  };
+
+  recognition.onend = () => {
+    setListening(false);  // end → FALSE
+  };
+
+
+  return (
+    <section className='w-full h-screen overflow-hidden px-4'>
+
+      <div className='h-full flex justify-evenly items-center flex-col'>
+        <div className=''>
+          <img src={assistantImg} alt="assistant" className='w-full max-w-[280px]' />
+        </div>
+        <div className='flex flex-col items-center gap-y-1'>
+          <p className='gradient-text text-2xl sm:text-3xl md:text-4xl font-semibold word-spacing text-center'>I'm Nora, Your Advanced Virtual Assistant</p>
+
+
+          {
+            speaking ? <div>
+              <img src={aiVoice} alt="" className='h-[200px] w-[550px]' />
+            </div> :
+              listening ?
+                <div className='flex flex-col items-center'>
+                  <div>
+                    <img src={listeningSvg} alt="" className='w-20 h-20' />
+                  </div>
+                  <p>listening</p>
+                </div> :
+                <div className='h-[104px] flex items-center'>
+                  <button className='cyan-btn cyan-btn:hover flex gap-x-1.5 items-center' onClick={handleClick}>Click Here <div><CiMicrophoneOn className='w-5 h-5' /></div> </button>
+                </div>
+          }
+        </div>
+      </div>
+
+    </section>
+  )
+}
+
+export default App
---
+++ src/context/AssistantContext.jsx
@@ -0,0 +1,85 @@
+import { createContext, useEffect, useState } from "react";
+
+export const AssistantContext = createContext()
+
+
+export const AssistantContextProvider = ({ children }) => {
+
+    const apiUrl = import.meta.env.VITE_GEMINI_URL
+    const [userPrompt, setUserPrompt] = useState("")
+    const [speaking, setSpeaking] = useState(false);
+
+    // speak text
+
+    const speak = (text) => {
+        const utterance = new SpeechSynthesisUtterance(text);
+        utterance.rate = 1;       // speed
+        utterance.pitch = 1;      // voice pitch
+        utterance.volume = 1;     // volume
+        utterance.lang = 'hi-IN'
+        window.speechSynthesis.cancel(); // old speech stop
+        window.speechSynthesis.speak(utterance);
+
+        utterance.onstart = () => {
+            setSpeaking(true); 
+        };
+
+        utterance.onend = () => {
+            setSpeaking(false); 
+        };
+
+    };
+
+    // listen ai text
+
+    const speechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    if (!speechRecognition) {
+        alert("Browser does not support Speech Recognition");
+        return;
+    }
+    const recognition = new speechRecognition();
+    recognition.onresult = (e) => {
+        setUserPrompt(e.results[0][0].transcript)
+        console.log(e.results[0][0].transcript)
+    }
+
+
+    const payload = {
+        "contents": [
+            {
+                "parts": [
+                    {
+                        "text": userPrompt
+                    }
+                ]
+            }
+        ]
+    }
+
+    const askQuestion = async () => {
+
+        if (userPrompt) {
+            const res = await fetch(apiUrl, {
+                method: "POST",
+                body: JSON.stringify(payload)
+            })
+
+            const data = await res.json()
+            speak(data.candidates[0].content.parts[0].text)
+        }
+
+
+    }
+
+    useEffect(() => {
+        if (userPrompt) {
+            askQuestion()
+        }
+    }, [userPrompt])
+
+    return (
+        <AssistantContext.Provider value={{ speak, recognition, askQuestion , speaking }} >
+            {children}
+        </AssistantContext.Provider>
+    )
+}
